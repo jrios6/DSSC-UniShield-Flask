fc1e8cfe34fe349a2f535fbbb3451d35e3342052
signapoop <leowyaoyang@gmail.com>
2017-05-06 23:02:04 +0800

    Random edits

--- insurance_policies_data.py
+++ insurance_policies_data.py
@@ -1,17 +1,26 @@
+
 item1 = dict()
-item1["url"] = "../static/img/mosquitopicture.png"
-item1["title"] = "Mosquito Insurance"
-item1["htitle"] = "#Mosquito Insurance"
-item1["summary"] = "Protection against Dengue fever, Zika fever, Chikungunya fever, Malaria & Yellow fever"
+item1["url"] = "../static/img/term_life.jpeg"
+item1["title"] = "Term Life Insurance"
+item1["htitle"] = "#Term Life Insurance"
+item1["summary"] = "Term pays out the sum assured in the event of death, terminal illness or total and permanent disability (TPD before age 65) during the term of the policy."
 item1["coverage"] = "$3000 or $1500 sum assured"
-item1["pricing"] = "From $31/year"
+item1["pricing"] = "Starting from $31/yr"
 
 item2 = dict()
-item2["url"] = "../static/img/mosquitopicture.png"
-item2["title"] = "Heart Insurance"
-item2["htitle"] = "#Mosquito Insurance"
-item2["summary"] = "Protection against Dengue fever, Zika fever, Chikungunya fever, Malaria & Yellow fever"
+item2["url"] = "../static/img/personal_accident.jpeg"
+item2["title"] = "Personal Accident Insurance"
+item2["htitle"] = "#Personal Accident Insurance"
+item2["summary"] = "Personal accident plans gives you comprehensive financial protection in times of needs. The policy includes accidental death and accidental permanent disablement cover, a lump sum bereavement grant, as well as hospitalisation allowance. "
 item2["coverage"] = "$3000 or $1500 sum assured"
-item2["pricing"] = "From $31/year"
+item2["pricing"] = "Starting from $31/yr"
 
-POLICIES = [item1, item2]
+item3 = dict()
+item3["url"] = "../static/img/mosquito.jpeg"
+item3["title"] = "Mosquito Insurance"
+item3["htitle"] = "#Mosquito Insurance"
+item3["summary"] = "Protection against Dengue fever, Zika fever, Chikungunya fever, Malaria & Yellow fever"
+item3["coverage"] = "$3000 or $1500 sum assured"
+item3["pricing"] = "Starting from $31/yr"
+
+POLICIES = [item1, item2, item3]
