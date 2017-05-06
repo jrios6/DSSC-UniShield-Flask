352ae08340e9bb3eef023077f125f38d80bbce37
signapoop <leowyaoyang@gmail.com>
2017-05-07 02:56:33 +0800

    Edits to modal

--- insurance_policies_data.py
+++ insurance_policies_data.py
@@ -3,24 +3,27 @@ item1 = dict()
 item1["url"] = "../static/img/term_life.jpeg"
 item1["title"] = "Term Life Insurance"
 item1["htitle"] = "#Term Life Insurance"
-item1["summary"] = "Term pays out the sum assured in the event of death, terminal illness or total and permanent disability (TPD before age 65) during the term of the policy."
+item1["summary"] = "Term Life Insurance pays out the sum assured in the event of death, terminal illness or total and permanent disability (TPD before age 65) during the term of the policy."
 item1["coverage"] = "$3000 or $1500 sum assured"
-item1["pricing"] = "Starting from $31/yr"
+item1["pricing"] = "Starting from $30/yr"
+item1["recommend"] = "Recommended because of dependents"
 
 item2 = dict()
 item2["url"] = "../static/img/personal_accident.jpeg"
 item2["title"] = "Personal Accident Insurance"
 item2["htitle"] = "#Personal Accident Insurance"
-item2["summary"] = "Personal accident plans gives you comprehensive financial protection in times of needs. The policy includes accidental death and accidental permanent disablement cover, a lump sum bereavement grant, as well as hospitalisation allowance. "
+item2["summary"] = "The personal accident plan gives you comprehensive financial protection in times of needs. The policy includes accidental death and accidental permanent disablement cover, a lump sum bereavement grant, as well as hospitalisation allowance. "
 item2["coverage"] = "$3000 or $1500 sum assured"
-item2["pricing"] = "Starting from $31/yr"
+item2["pricing"] = "Starting from $40/yr"
+item2["recommend"] = "Recommended because of past history of accidents"
 
 item3 = dict()
 item3["url"] = "../static/img/mosquito.jpeg"
 item3["title"] = "Mosquito Insurance"
 item3["htitle"] = "#Mosquito Insurance"
-item3["summary"] = "Protection against Dengue fever, Zika fever, Chikungunya fever, Malaria & Yellow fever"
+item3["summary"] = "The mosquito insurance plan gives you protection against dengue fever, Zika fever, Chikungunya fever, malaria & yellow fever."
 item3["coverage"] = "$3000 or $1500 sum assured"
-item3["pricing"] = "Starting from $31/yr"
+item3["pricing"] = "Starting from $15/yr"
+item3["recommend"] = "Recommended because of Dengue Danger Level of home address"
 
 POLICIES = [item1, item2, item3]
