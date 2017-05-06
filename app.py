aef82fd2afcbaabf4cb53018e924e1a278e848f1
Ong Jia Rui <ongjiarui@hotmail.com>
2017-05-06 21:46:34 +0800

    Edit 2

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from insurance_policies_data import POLICIES
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def start():
     return render_template('start.html')
@@ -25,7 +24,6 @@ def mypolicies():
 @app.route('/bot')
 def bot():
     return render_template('bot.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000, host='0.0.0.0')
