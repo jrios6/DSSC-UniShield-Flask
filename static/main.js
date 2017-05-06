8b8d1501f4c7139ceeb90596f13d4162f76b6af8
signapoop <leowyaoyang@gmail.com>
2017-05-07 02:08:18 +0800

    edittable

--- static/main.js
+++ static/main.js
@@ -3,23 +3,46 @@ $(document).ready(function() {
     $.fn.editable.defaults.mode = 'popup';
 
     //make username editable
-    $('#username').editable();
+    $('#name').editable();
 
     //make status editable
-    $('#status').editable({
+    $('#gender').editable({
         type: 'select',
-        title: 'Select status',
         placement: 'right',
-        value: 2,
+        value: 1,
         source: [
-            {value: 1, text: 'status 1'},
-            {value: 2, text: 'status 2'},
-            {value: 3, text: 'status 3'}
+            {value: 1, text: 'Male'},
+            {value: 2, text: 'Female'}
         ]
-        /*
-        //uncomment these lines to send data on server
-        ,pk: 1
-        ,url: '/post'
-        */
+    });
+    $('#dob').editable({
+        format: 'YYYY-MM-DD',
+        viewformat: 'DD.MM.YYYY',
+        template: 'D / MMMM / YYYY',
+        combodate: {
+                minYear: 1900,
+                maxYear: 2017,
+                minuteStep: 1
+           }
+    });
+    $('#address').editable();
+    $('#smoke').editable({
+        type: 'select',
+        placement: 'right',
+        value: 1,
+        source: [
+            {value: 1, text: 'Non-smoker'},
+            {value: 2, text: 'Smoker'}
+        ]
+    });
+    $('#marry').editable({
+        type: 'select',
+        placement: 'right',
+        value: 1,
+        source: [
+            {value: 1, text: 'Single'},
+            {value: 2, text: 'Engaged'},
+            {value: 3, text: 'Married'}
+        ]
     });
 });
